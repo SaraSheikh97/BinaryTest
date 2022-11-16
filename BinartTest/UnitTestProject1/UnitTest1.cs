c39901572a2b675c656c9490b6743d39457f2ff3
LENOVO <LENOVO@DESKTOP-TFGVT36>
2022-11-16 05:12:06 +0300

    Unit Test Program

---
+++ BinartTest/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BinartTest;
+using BinartTest.BinaryClass;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestBinary4()
+        {
+            BinaryConverter p = new BinaryConverter();
+            string expected = "100";
+            string actual = p.IntToBinaryString(4);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
